fcc86987346f9f8a266f8ffb0a10b2f2272fc276
Hikikomori-Kim <gsb1028@naver.com>
2025-03-25 20:14:07 +0900

    프로젝트생성1

---
+++ academyfinal/src/main/java/com/hiki/academyfinal/AcademyfinalApplication.java
@@ -0,0 +1,13 @@
+package com.hiki.academyfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyfinalApplication.class, args);
+	}
+
+}
---
+++ academyfinal/src/test/java/com/hiki/academyfinal/AcademyfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiki.academyfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
