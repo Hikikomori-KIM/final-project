70284c999ebf3a76c04c08ae03631dab685f53d7
hxxggu <hxxggu@gmail.com>
2025-04-24 17:56:55 +0900

    공지사항 끗

--- academyfinal/src/main/java/com/hiki/academyfinal/restcontroller/NoticeRestController.java
+++ academyfinal/src/main/java/com/hiki/academyfinal/restcontroller/NoticeRestController.java
@@ -5,15 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hiki.academyfinal.dao.NoticeDao;
 import com.hiki.academyfinal.dto.NoticeDto;
+import com.hiki.academyfinal.error.TargetNotFoundException;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/notice")
+@RequestMapping("/api/service/notice")
 public class NoticeRestController {
 	
 	@Autowired
@@ -24,6 +26,10 @@ public class NoticeRestController {
 		return noticeDao.selectList();
 	}
 	
-	
-	
+	@GetMapping("/{noticeNo}")
+	public NoticeDto detail(@PathVariable long noticeNo) {	
+		NoticeDto noticeDto = noticeDao.selectOne(noticeNo);
+		if(noticeDto == null) throw new TargetNotFoundException();
+		return noticeDto;
+	}	
 }
