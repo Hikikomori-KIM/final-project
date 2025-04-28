90f47b34489ea0e01a68bf2b7cdcad990e924dfb
hxxggu <hxxggu@gmail.com>
2025-04-28 17:45:26 +0900

    웹소켓 진행중

---
+++ academyfinal/src/main/java/com/hiki/academyfinal/dto/MessageViewDto.java
@@ -0,0 +1,23 @@
+package com.hiki.academyfinal.dto;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class MessageViewDto {
+	private Long messageNo;
+	private String messageType;
+	private String messageContent;
+	private Timestamp messageTime;
+	private String messageSender;
+	private String messageReceiver;
+	
+	private String senderName;
+	private String senderType;
+	private String receiverName;
+	private String receiverType;
+}
