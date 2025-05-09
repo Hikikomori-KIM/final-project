f0327ea90b6077d5e6896aff100d85c24bede50c
Hikikomori-Kim <gsb1028@naver.com>
2025-05-09 13:11:58 +0900

    장바구니 상품이 있을때 상품수정 ,삭제가 불가능했던 현상 해결

--- academyfinal/src/main/java/com/hiki/academyfinal/dto/VolumeDto.java
+++ academyfinal/src/main/java/com/hiki/academyfinal/dto/VolumeDto.java
@@ -7,10 +7,12 @@ import lombok.NoArgsConstructor;
 
 @Data @AllArgsConstructor @NoArgsConstructor @Builder
 public class VolumeDto {
-	private long volumeNo; //시퀀스
-	private long productNo; // fk
-	private String volumeMl; //ex: 50ml
+	private long volumeNo; // 시퀀스
+	private long productNo; // FK
+	private String volumeMl; // 예: 50ml
 	private int volumeStock; // 각 용량별 재고
-	private Integer volumePrice; // 추가\
-	private Integer discountedVolumePrice;
+	private Integer volumePrice; // 원가
+	private Integer discountedVolumePrice; // 할인 가격
+
+	private String volumeStatus; // ✅ 상태값: active, inactive, deleted
 }
